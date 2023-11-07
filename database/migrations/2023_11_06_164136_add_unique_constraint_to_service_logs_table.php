a3dabdb1dcbcc62b8d7b7f9dccfde4e73dd346b7
shanto150 <shanto150@gmail.com>
2023-11-07 11:32:15 +0600

    uniqcon

--- database/migrations/2023_11_06_164136_add_unique_constraint_to_service_logs_table.php
+++ database/migrations/2023_11_06_164136_add_unique_constraint_to_service_logs_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('service_logs', function (Blueprint $table) {
-            //
+            $table->dropUnique(['outlet_code', 'visi_id','log_date']);
         });
     }
 };
